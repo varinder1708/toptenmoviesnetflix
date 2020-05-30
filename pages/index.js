3cbdbae8a2fcd8288abaee99d0e16e205569b426
Varinder Singh <Varinder.Singh@timesinternet.in>
2020-05-30 09:49:06 +0530

    changing title

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,24 @@
+// import fs from 'fs'
+// import path from 'path'
+import Head from 'next/head'
+import {data,heading} from '../data/upcomingshows';
+
 import Link from "next/link";
 import Layout from "../components/Layout";
-
-const Index = () => (
+import Heading from "../components/Heading";
+import Listing from "../components/Listing";
+const listing = () => (
     <Layout>
-      <br />
-      <Link href="/explore">
-        <a> Welcome to WHATABYTE! Start Exploring Now</a>
-      </Link>
+      <Head>
+        <title>{heading}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+ <Heading title={heading}/>
+ <Listing data={data}/>
     </Layout>
   );
 
-export default Index;
+
+
+
+export default listing
