310892cfa267bcbd6bff8d763473c3eaae296710
Varinder Singh <Varinder.Singh@timesinternet.in>
2020-06-20 21:20:14 +0530

    adding gpt script

--- components/Layout.js
+++ components/Layout.js
@@ -25,6 +25,11 @@ const Layout = props => {
     
     <main>
       <Head>
+      <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','UA-170166482-1');`}} />
 <title>{props.heading} | Trishn</title>
 <meta http-equiv="Last-Modified" content="Saturday, 20 June, 2020 07:15:29AM"/>
 <meta name="Last-Modified" content="Saturday, 20 June, 2020 07:15:29AM"/>
