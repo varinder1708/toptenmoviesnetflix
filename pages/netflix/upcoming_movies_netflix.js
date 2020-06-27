ac92aec90fa9b7762409432aeeaf3b65ce1f1b3a
Varinder Singh <Varinder.Singh@timesinternet.in>
2020-06-27 10:02:20 +0530

    removing env

--- pages/netflix/upcoming_movies_netflix.js
+++ pages/netflix/upcoming_movies_netflix.js
@@ -1,11 +1,11 @@
 // import fs from 'fs' import path from 'path'
 
-import {data, heading} from '../../../data/upcomingshows';
+import {data, heading} from '../../data/upcomingshows';
 
 import Link from "next/link";
-import Layout from "../../../components/Layout";
-import Heading from "../../../components/Heading";
-import Listing from "../../../components/Listing";
+import Layout from "../../components/Layout";
+import Heading from "../../components/Heading";
+import Listing from "../../components/Listing";
 const listing = () => (
   <Layout layout="singlecolumnlayout" heading={heading}>
   
