ae5904966c8405188dff8a8990090af09e93a72b
Varinder Singh <Varinder.Singh@timesinternet.in>
2020-07-11 23:11:37 +0530

    adding menu everywhere

--- pages/travel/goa/waterfalls-in-goa/dudhsagar-goa-waterfall.js
+++ pages/travel/goa/waterfalls-in-goa/dudhsagar-goa-waterfall.js
@@ -6,7 +6,7 @@ import Layout from "components/Layout";
 import Heading from "components/Heading";
 import Show from "components/Show";
 import TopPicks from "components/TopPicks";
-import {nav_config} from "../../../config/constants";
+import {nav_config} from "config/constants";
 const listing = () => (
   <Layout layout="" heading={heading} showsidebar="true" navbar={nav_config}>
     {/* <Heading title={heading}/> */}
