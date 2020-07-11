59f34795323e5e04d823d745bebcc33b7bf35cff
Varinder Singh <Varinder.Singh@timesinternet.in>
2020-07-11 23:13:20 +0530

    adding menu everywhere

--- pages/travel/goa/top_beaches_in_goa.js
+++ pages/travel/goa/top_beaches_in_goa.js
@@ -7,10 +7,10 @@ import Layout from "../../../components/Layout";
 import Heading from "../../../components/Heading";
 import Listing from "../../../components/Listing";
 import TopPicks from "../../../components/TopPicks";
-import {nav_config} from "../../../config/constants";
+import {nav_config} from "config/constants";
 const canonical="https://trishn.com/travel/goa/top-beaches-in-goa";
 const listing = () => (
-  <Layout layout="threecolumnlayout" heading={heading} navbar={config} canoical={canonical}>
+  <Layout layout="threecolumnlayout" heading={heading} navbar={nav_config} canoical={canonical}>
     {/* <Heading title={heading}/> */}
     <Listing   data={data} layout="threecolumnlayout"/>
     <TopPicks showheading="true"    layout="threecolumnlayout"/>
