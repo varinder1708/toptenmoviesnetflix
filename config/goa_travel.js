dbc537abd06a81ce753998a0a027ae49220169bf
Varinder Singh <Varinder.Singh@timesinternet.in>
2020-07-11 14:40:22 +0530

    uderscore removed

--- config/goa_travel.js
+++ config/goa_travel.js
@@ -3,15 +3,15 @@ const config = {
     navButtons : [
       {
         label: "Waterfalls in Goa",
-        path: "/travel/goa/waterfalls_in_goa",
+        path: "/travel/goa/waterfalls-in-goa",
         //   icon: <FontAwesomeIcon icon={faCompass} />
       }, {
         label: "Top 26 beaches in Goa",
-        path: "/travel/goa/top_goa_beaches",
+        path: "/travel/goa/top_-goa-beaches",
         //   icon: <FontAwesomeIcon icon={faMapMarkerAlt} />
       }, {
         label: "Top 10 beaches in Goa",
-        path: "/travel/goa/top_beaches_in_goa",
+        path: "/travel/goa/top-beaches-in-goa",
         //   icon: <FontAwesomeIcon icon={faShoppingCart} />
       }
       // {   label: "Profile",   path: "/profile",   icon: <FontAwesomeIcon
