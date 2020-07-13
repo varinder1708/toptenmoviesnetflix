6adda85a25fffff660bd087721d053e7ea002f61
Varinder Singh <Varinder.Singh@timesinternet.in>
2020-07-13 22:08:01 +0530

    hippie

--- pages/travel/hidden-gems-of-india.js
+++ pages/travel/hidden-gems-of-india.js
@@ -1,12 +1,12 @@
 // import fs from 'fs' import path from 'path'
 import Head from 'next/head'
 import Link from "next/link";
-import {data, heading} from '../../../data/travel/hidden-gems-of-india';
-import Layout from "../../../components/Layout";
-import Heading from "../../../components/Heading";
-import Listing from "../../../components/Listing";
-import TopPicks from "../../../components/TopPicks";
-import {nav_config} from "../../../config/constants";
+import {data, heading} from 'data/travel/hidden-gems-of-india';
+import Layout from "components/Layout";
+import Heading from "components/Heading";
+import Listing from "components/Listing";
+import TopPicks from "components/TopPicks";
+import {nav_config} from "config/constants";
 const listing = () => (
   <Layout layout="" heading={heading} showsidebar="true" navbar={nav_config}>
     {/* <Heading title={heading}/> */}
