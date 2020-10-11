313ad15fc06bb5357598943401086417324d21f2
Varinder Singh <Varinder.Singh@timesinternet.in>
2020-10-11 13:40:26 +0530

    cache

--- pages/travel/goa/waterfalls-in-goa.js
+++ pages/travel/goa/waterfalls-in-goa.js
@@ -7,7 +7,6 @@ import Layout from "../../../components/Layout";
 import Heading from "../../../components/Heading";
 import Listing from "../../../components/Listing";
 import TopPicks from "../../../components/TopPicks";
-import {nav_config} from "../../../config/constants";
 const listing = () => (
   <Layout data={data}>
     {/* <Heading title={heading}/> */}
