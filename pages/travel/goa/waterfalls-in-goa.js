91276541f64caff7fa29ef20203d7e5f0f090831
Varinder Singh <Varinder.Singh@timesinternet.in>
2020-10-11 13:36:11 +0530

    removing pages and updating date

--- pages/travel/goa/waterfalls-in-goa.js
+++ pages/travel/goa/waterfalls-in-goa.js
@@ -2,13 +2,12 @@
 import Head from 'next/head'
 import Link from "next/link";
 import * as data from '../../../data/waterfalls';
-import{site} from 'config/constants';
+
 import Layout from "../../../components/Layout";
 import Heading from "../../../components/Heading";
 import Listing from "../../../components/Listing";
 import TopPicks from "../../../components/TopPicks";
 import {nav_config} from "../../../config/constants";
-
 const listing = () => (
   <Layout data={data}>
     {/* <Heading title={heading}/> */}
