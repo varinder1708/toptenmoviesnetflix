76e2c0978440bdabbb2d4a4e3554db1c512af128
Varinder Singh <Varinder.Singh@timesinternet.in>
2020-10-18 07:48:28 +0530

    footer

---
+++ data/sample.js
@@ -0,0 +1,17 @@
+import{site} from 'config/constants';
+export const canonical=`${site}/travel/`;
+export const keywords = "";
+export const heading = "";
+export const data = [
+  {
+    "link": "",
+    "name": "",
+    "image":[{name:'',src:"travel/hippie-destinations-india/goa2.jpg"}] ,
+    "desc": '',
+    "best": "",
+    "nrs": "",
+    "na": "",
+    "htr": ""
+  }
+
+]
