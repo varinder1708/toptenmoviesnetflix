6c212496d894f2354719aa53a0978e57863486e1
varinder1708 <varinder.sadana@gmail.com>
2021-08-12 21:43:52 +0530

    pachmarhi

--- components/Listing.js
+++ components/Listing.js
@@ -1,97 +1,116 @@
-import LazyImage from './LazyImage'
-import LoadCarousel from './LoadCarousel'
+import LazyImage from "./LazyImage";
+import LoadCarousel from "./LoadCarousel";
 import "./Listing.scss";
 import Link from "next/link";
 
 const Listing = (props) => (
   <ul class={`${props.layout} fullscreen_images`}>
-    {props
-      .data
-      .map((post, i) => (
+    {props.data &&
+      props.data.map((post, i) => (
         <li>
-          {post.name && props.layout!="homelayout" && (
+          {post.name && props.layout != "homelayout" && (
             <h3>
-              {post.link
-                ? <Link href={post.link}>
-                    <a>{i + 1}. {post.name}</a>
-                  </Link>
-                : `${i + 1}. ${post.name}`}
+              {post.link ? (
+                <Link href={post.link}>
+                  <a>
+                    {i + 1}. {post.name}
+                  </a>
+                </Link>
+              ) : (
+                `${i + 1}. ${post.name}`
+              )}
               {/* {i + 1}.{post.name} */}
             </h3>
           )}
 
           {post.image && Array.isArray(post.image) && (
-            <LoadCarousel data={post.image}/>
-           
+            <LoadCarousel data={post.image} />
           )}
-           
-           {post.image && (!Array.isArray(post.image)) && (
+
+          {post.image && !Array.isArray(post.image) && (
             <p class="img">
-              <LazyImage src={`/images/${post.image}`} alt={post.name}/> {/* <img src={`/images/${post.image}`}/> */}
+              <LazyImage src={`/images/${post.image}`} alt={post.name} />{" "}
+              {/* <img src={`/images/${post.image}`}/> */}
             </p>
           )}
-            {post.name && props.layout=="homelayout" && (
-             <h3>
-             {post.link
-               ? <Link href={post.link}>
-                   <a>{post.name}</a>
-                 </Link>
-               : `${i + 1}. ${post.name}`}
-             {/* {i + 1}.{post.name} */}
-           </h3>
-           )}
+          {post.name && props.layout == "homelayout" && (
+            <h3>
+              {post.link ? (
+                <Link href={post.link}>
+                  <a>{post.name}</a>
+                </Link>
+              ) : (
+                `${i + 1}. ${post.name}`
+              )}
+              {/* {i + 1}.{post.name} */}
+            </h3>
+          )}
           {post.i && (
             <div
               class="insta"
               dangerouslySetInnerHTML={{
-              __html: post.i
-            }}></div>
+                __html: post.i,
+              }}
+            ></div>
           )}
           {post.type && (
-            <p class="type">{post.type}
-              ({post.date})</p>
+            <p class="type">
+              {post.type}({post.date})
+            </p>
           )}
           {post.desc && (
-            <div class="desc" dangerouslySetInnerHTML={{
-              __html: post.desc
-            }}></div>
+            <div
+              class="desc"
+              dangerouslySetInnerHTML={{
+                __html: post.desc,
+              }}
+            ></div>
           )}
           {post.best && (
             <div class="na">
-              <strong>Best Time to Visit</strong>: {`${post.best}`}</div>
+              <strong>Best Time to Visit</strong>: {`${post.best}`}
+            </div>
           )}
 
           {post.na && (
             <div class="na">
-              <strong>Nearest Airport</strong>: {`${post.na}`}</div>
+              <strong>Nearest Airport</strong>: {`${post.na}`}
+            </div>
           )}
           {post.nra && (
             <div class="nra">
-              <strong>Nearest Railway Station</strong>: {`${post.nra}`}</div>
+              <strong>Nearest Railway Station</strong>: {`${post.nra}`}
+            </div>
           )}
           {post.nrs && (
             <div class="nrs">
-              <strong>Nearest Railway Station</strong>: {`${post.nrs}`}</div>
+              <strong>Nearest Railway Station</strong>: {`${post.nrs}`}
+            </div>
           )}
           {post.htr && (
             <div class="nra">
-              <strong>How to Reach</strong>: {`${post.htr}`}</div>
+              <strong>How to Reach</strong>: {`${post.htr}`}
+            </div>
           )}
           {post.lt && (
             <div class="nra">
-              <strong>Local Transport</strong>: {`${post.lt}`}</div>
+              <strong>Local Transport</strong>: {`${post.lt}`}
+            </div>
           )}
           {post.ptv && (
             <div class="nra">
-              <strong>Places to Visit</strong>: {`${post.ptv}`}</div>
+              <strong>Places to Visit</strong>: {`${post.ptv}`}
+            </div>
           )}
           {post.ef && (
             <div class="nra">
-              <strong>Entry Free</strong>: {`${post.ef}`}</div>
+              <strong>Entry Free</strong>: {`${post.ef}`}
+            </div>
           )}
           {post.tm && (
             <div class="nra">
-              <strong>Timings</strong>: {`${post.tm}`}</div>
+              <strong>Timings</strong>: {`${post.tm}`}
+            </div>
           )}
         </li>
       ))}
--- utils/fixSidebar.js
+++ utils/fixSidebar.js
@@ -1,55 +1,60 @@
-import{site} from 'config/constants';
+import { site } from "config/constants";
 
-export async function apicall(url)
- {
-    const res = await fetch(`${site}${url}`);
-    const json = await res.json();
-    return {
-      props: {
-        data: json,
-      },
-    };
- }
- export  function fixSidebar(id) {
-    var e = $(id);
-    var lastScrollTop = 0;
-    var firstOffset = e.offset().top;
-    var lastA = e.offset().top;
-    var isFixed = false;
-    $(window).scroll(function(event){
-        if (isFixed) {
-            return;
+export async function apicall(url) {
+  console.log("bbbbbbbb");
+  const res = await fetch(`${site}${url}`);
+  let json = {};
+  try {
+    json = await res.json();
+  } catch (e) {}
+  return {
+    props: {
+      data: json,
+    },
+  };
+}
+export function fixSidebar(id) {
+  var e = $(id);
+  var lastScrollTop = 0;
+  var firstOffset = e.offset().top;
+  var lastA = e.offset().top;
+  var isFixed = false;
+  $(window).scroll(function (event) {
+    if (isFixed) {
+      return;
+    }
+    var a = e.offset().top;
+    var b = e.height();
+    var c = $(window).height();
+    var d = $(window).scrollTop();
+    if (b <= c - a) {
+      e.css({ position: "fixed" });
+      isFixed = true;
+      return;
+    }
+    if (d > lastScrollTop) {
+      // scroll down
+      if (e.css("position") != "fixed" && c + d >= a + b) {
+        e.css({ position: "fixed", bottom: 0, top: "auto" });
+      }
+      if (a - d >= firstOffset) {
+        e.css({ position: "absolute", bottom: "auto", top: lastA });
+      }
+    } else {
+      // scroll up
+      if (a - d >= firstOffset) {
+        if (e.css("position") != "fixed") {
+          e.css({ position: "fixed", bottom: "auto", top: firstOffset });
         }
-        var a = e.offset().top;
-        var b = e.height();
-        var c = $(window).height();
-        var d = $(window).scrollTop();
-        if (b <= c - a) {
-            e.css({position: "fixed"});
-            isFixed = true;
-            return;
-        }			
-        if (d > lastScrollTop){ // scroll down
-            if (e.css("position") != "fixed" && c + d >= a + b) {
-                e.css({position: "fixed", bottom: 0, top: "auto"});
-            }
-            if (a - d >= firstOffset) {
-                e.css({position: "absolute", bottom: "auto", top: lastA});
-            }
-        } else { // scroll up
-            if (a - d >= firstOffset) {
-                if (e.css("position") != "fixed") {
-                    e.css({position: "fixed", bottom: "auto", top: firstOffset});
-                }
-            } else {
-                if (e.css("position") != "absolute") {
-                    e.css({position: "absolute", bottom: "auto", top: lastA});
-                }               
-            }
+      } else {
+        if (e.css("position") != "absolute") {
+          e.css({ position: "absolute", bottom: "auto", top: lastA });
         }
-        lastScrollTop = d;
-        lastA = a;
-    });
+      }
+    }
+    lastScrollTop = d;
+    lastA = a;
+  });
 }
 
 //fixMe("#stick");
